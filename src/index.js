56fa163442db8df97082c5c20a45b18a4a435dd9
Gabriel Davi <gdavi.gd@gmail.com>
2020-03-23 21:23:12 -0300

    Setting up sockets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-const express = require('express'); 
-const mongoose = require('mongoose');
-const app = express(); 
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const http = require("http");
+const routes = require("./routes");
+const cors = require("cors");
+const { setupWebsocket } = require("./websocket");
 
-mongoose.connect('mongodb+srv://oministack:gabriel299@cluster0-ldd1j.mongodb.net/week10?retryWrites=true&w=majority', { 
+const app = express();
+const server = http.Server(app); //extraindo o servidor http do express
+setupWebsocket(server);
+
+mongoose.connect(
+  "mongodb+srv://oministack:gabriel299@cluster0-ldd1j.mongodb.net/week10?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
- });
+  }
+);
 
- app.use(express.json()); 
+app.use(express.json());
+app.use(cors());
+
 app.use(routes);
 
-app.listen(3333);
-
+app.listen(3000);
