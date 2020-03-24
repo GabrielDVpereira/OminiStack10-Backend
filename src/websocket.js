99a068b66e8a9164e6582afa004ab6abcdd194e3
Gabriel Davi <gdavi.gd@gmail.com>
2020-03-24 19:55:56 -0300

    Adding websocket

--- src/websocket.js
+++ src/websocket.js
@@ -1,8 +1,35 @@
 const socketio = require("socket.io");
-
+const parseArray = require("./utils/ParseToArray");
+const calculateDistance = require("./utils/calculateDistance");
+const connections = [];
+let io;
 exports.setupWebsocket = server => {
-  const io = socketio(server);
+  io = socketio(server);
+  io.listen(4001);
   io.on("connection", socket => {
-    console.log(socket.id);
+    const { latitude, longitute, techs } = socket.handshake.query;
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitute: Number(longitute)
+      },
+      techs: parseArray(techs)
+    });
+  });
+};
+
+exports.findConnections = (coordinates, techs) => {
+  return connections.filter(conn => {
+    return (
+      calculateDistance(coordinates, conn.coordinates) < 10 &&
+      conn.techs.some(item => techs.include(items))
+    );
+  });
+};
+
+exports.sendMessage = (to, message, data) => {
+  to.forEach(connection => {
+    io.to(connection.id).emit(message, data);
   });
 };
